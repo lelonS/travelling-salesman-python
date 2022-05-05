fe33c515b16a0267ee9bcd338487896fff994ab0
lelonS <35070147+lelonS@users.noreply.github.com>
2022-05-05 19:16:25 +0200

    Make screen

--- travelling_salesman.py
+++ travelling_salesman.py
@@ -1,3 +1,35 @@
 import pygame
 import math
 import random
+
+
+def update():
+    pygame.display.flip()
+
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Window dimensions
+WIDTH = 500
+HEIGHT = 500
+
+# Make screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(BLACK)
+update()
+
+
+# Loop
+running = True
+
+while running:
+    # Get events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Quit
+print("Quitting")
+pygame.quit()
